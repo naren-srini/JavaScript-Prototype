4b23ef5453e5634ffdcf93464f583a204915cd69
Naren <prasannanarayanan2000@gmail.com>
2021-04-05 17:18:38 -0400

    commit

--- 01_05/script.js
+++ 01_05/script.js
@@ -8,34 +8,34 @@
 See! this line is also commented out! */
 
 const updateBackpack = (update) => {
-  let main = document.querySelector("main"); // main is an element
-  main.innerHTML = markup(backpack);
-  console.info(update);
+    let main = document.querySelector("main"); // main is an element
+    main.innerHTML = markup(backpack);
+    console.info(update);
 };
 
 const backpack = {
-  name: "Everyday Backpack",
-  volume: 30,
-  color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
-  },
-  lidOpen: false,
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
-    updateBackpack(`Lid status changed.`);
-  },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-    updateBackpack(`Strap lengths updated.`);
-  },
+    name: "Everyday Backpack",
+    volume: 30,
+    color: "grey",
+    pocketNum: 15,
+    strapLength: {
+        left: 26,
+        right: 26,
+    },
+    lidOpen: false,
+    toggleLid: function(lidStatus) {
+        this.lidOpen = lidStatus;
+        updateBackpack(`Lid status changed.`);
+    },
+    newStrapLength: function(lengthLeft, lengthRight) {
+        this.strapLength.left = lengthLeft;
+        this.strapLength.right = lengthRight;
+        updateBackpack(`Strap lengths updated.`);
+    },
 };
 
 const markup = (backpack) => {
-  return `
+    return `
   <div>
 	<h3>${backpack.name}</h3>
 	<ul>
