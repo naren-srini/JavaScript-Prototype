fa7139e6dc56c30b108ab87a1741aad062765b7b
Naren <prasannanarayanan2000@gmail.com>
2021-04-06 15:27:24 -0400

    commit all

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,34 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+var smartphone = {
+    name: "Samsung",
+    model: "Galaxy",
+    version: "S21",
+    color: "white, black, grey",
+    battery: 4000,
+    size: {
+        length: 6.2,
+        width: 3.5,
+    },
+    display: {
+        screenHW: {
+            "Dynamic AMOLED",
+            screenRES: {
+                pixelL: 2400,
+                pixelW: 1080
+                screenRatio: ["20:9"],
+                PPI: 441,
+            },
+        },
+        processor: "Qualcomm",
+        graphics: "Adreno",
+        fullName: function phoneName() {
+            return (this.name + " " + this.model + " " + this.version);
+        },
+
+    },
+};
+
+console.log("The name of object:", smartphone.fullName("phoneName"));
