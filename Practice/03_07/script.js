a77ccedea040522e45f0c0afc94e94ad01a10fab
Naren <prasannanarayanan2000@gmail.com>
2021-04-06 16:21:10 -0400

    commit

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -8,11 +8,14 @@
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
 
-var smartphone = {
+var device = {
+
+    deviceType: "Smartphone",
     name: "Samsung",
-    model: "Galaxy",
+    series: "Galaxy",
     version: "S21",
     color: "white, black, grey",
+
     price: {
         usd: (800, "US$"),
         cad: (1000, "CAD$"),
@@ -27,36 +30,46 @@ var smartphone = {
         rear3: (12, "MP"),
         front: (10, "MP"),
     },
-    battery: (4000, "mAh"),
-    RAM: (8, "GB LPDDR5"),
-    storage: (128, "GB (UFS 3.0)"),
-    processor: {
+    hardware: {
         manufacturer: "Qualcomm",
         series: "Snapdragon",
         model: 888,
-        graphics: "Adreno",
+        graphics: ("Adreno", 660),
+        RAM: (8, "GB LPDDR5"),
+        storage: (128, "GB (UFS 3.0)"),
     },
     size: {
         length: 6.2,
         width: 3.5,
     },
-
+    batteryTech: {
+        capacity: (4000, "mAh"),
+        type: "Li-Polymer",
+        userReplacable: false,
+    },
+    screenRES: {
+        pixelL: 2400,
+        pixelW: 1080,
+        screenRatio: ["20:9"],
+        PPI: 441,
+    },
     display: {
-        screenHW: "Dynamic AMOLED",
+        screenTech: "Dynamic AMOLED",
         refreshRate: (120, "Hz"),
-        screenRES: {
-            pixelL: 2400,
-            pixelW: 1080,
-            screenRatio: ["20:9"],
-            PPI: 441,
-        },
+        screenToBody: 86,
+        peakBrightness: (1300, "cd/m2 (nit)"),
     },
-
+    screenRes: {
+        pixelL: 2400,
+        pixelW: 1080,
+        screenRatio: "20:9",
+        PPI: 441,
+    },
     fullName: function phoneName() {
-        return (this.name + " " + this.model + " " + this.version);
+        return this.name + " " + this.series + " " + this.version
     },
-
 
 };
 
-console.log("The name of object:", smartphone.fullName("phoneName"));
+
+console.log("The device name is", device.fullName());
