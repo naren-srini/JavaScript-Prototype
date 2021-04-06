0eaea22c6a690d4030a73b6b24b5ed34d3f0e75d
Naren <prasannanarayanan2000@gmail.com>
2021-04-06 14:27:50 -0400

    commit all

--- 03_06/script.js
+++ 03_06/script.js
@@ -3,22 +3,28 @@
  */
 
 const backpack = {
-  name: "Everyday Backpack",
-  volume: 30,
-  color: "grey",
-  pocketNum: 15,
-  strapLength: {
-    left: 26,
-    right: 26,
-  },
-  lidOpen: false,
-  toggleLid: function (lidStatus) {
-    this.lidOpen = lidStatus;
-  },
-  newStrapLength: function (lengthLeft, lengthRight) {
-    this.strapLength.left = lengthLeft;
-    this.strapLength.right = lengthRight;
-  },
+    name: "Everyday Backpack",
+    volume: 30,
+    color: "grey",
+    pocketNum: 15,
+    strapLength: {
+        left: 26,
+        right: 26,
+    },
+    lidOpen: false,
+    toggleLid: function(lidStatus) {
+        this.lidOpen = lidStatus;
+    },
+    newStrapLength: function(lengthLeft, lengthRight) {
+        this.strapLength.left = lengthLeft;
+        this.strapLength.right = lengthRight;
+    },
 };
 
 console.log("The backpack object:", backpack);
+console.log("The pocket number value", backpack.pocketNum);
+console.log("Strap Length", backpack.strapLength);
+
+var query = "pocketNum";
+
+console.log("The Pocket Num Value:", backpack[query]);
