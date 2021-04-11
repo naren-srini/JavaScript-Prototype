74ebfc8e6c85ffeb146f8bc9179e84bc134648dd
Naren <prasannanarayanan2000@gmail.com>
2021-04-10 20:00:39 -0400

    commit

--- 08_03/script.js
+++ 08_03/script.js
@@ -4,15 +4,15 @@
  */
 
 const greenPack = {
-  name: "Frog Pack",
-  color: "green",
-  volume: 8,
-  pocketNum: 3,
+    name: "Frog Pack",
+    color: "green",
+    volume: 8,
+    pocketNum: 3,
 };
 
-const addPack = function (currentPack) {
-  const newArticle = document.createElement("article");
-  newArticle.innerHTML = `
+const addPack = function(currentPack) {
+    const newArticle = document.createElement("article");
+    newArticle.innerHTML = `
     <h1>${currentPack.name}</h1>
     <ul>
       <li>Volume: ${currentPack.volume}</li>
@@ -20,8 +20,10 @@ const addPack = function (currentPack) {
       <li>Number of pockets: ${currentPack.pocketNum}</li>
     </ul>
   `;
-  return newArticle;
+    return newArticle;
 };
 
+const theArticle = addPack(greenPack);
+console.log(theArticle);
 const main = document.querySelector("main");
 main.append(addPack(greenPack));
