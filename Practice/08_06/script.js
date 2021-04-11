899eb3745917aa4c4524b6b53678468c4df31aac
Naren <prasannanarayanan2000@gmail.com>
2021-04-10 22:47:29 -0400

    commit

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,11 +5,12 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
-window.price = 1000;
+window.priceUSD = 1000;
 window.inr = 55;
 
 function tax(price, tax = 0.13) {
-    let finalPrice = price * tax + price;
+    let priceCAD = 1200;
+    let finalPrice = priceCAD * tax + price;
     return finalPrice;
 }
 const laptop = {
@@ -20,4 +21,27 @@ const laptop = {
     size: 14,
     price: 999,
     tax: 0.13,
+    storage: {
+        ram: 8,
+        rom: 128,
+    },
+    newStoragesize: function(ReadOnlyMem, RandomAccessMem) {
+        this.storage.rom = ReadOnlyMem;
+        this.storage.ram = RandomAccessMem;
+        console.log(
+            "Storage of ROM and RAM is",
+            ReadOnlyMem,
+            "GB",
+            RandomAccessMem,
+            "GB"
+        );
+    },
 };
+laptop.newStoragesize(16, 512);
+
+console.log("Price in USD Before Taxes", laptop.price);
+// console.log("Price in CAD After Taxes", tax.finalPrice());
+console.log("The price of Laptop in CAD:", window.finalPrice);
+console.log("New price in inr:", this.price_in_inr);
+
+console;
