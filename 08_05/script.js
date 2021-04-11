17fb86b56f6d2ea541ebc6ee1b06b0391aaf05b2
Naren <prasannanarayanan2000@gmail.com>
2021-04-10 21:29:54 -0400

    commit

--- 08_05/script.js
+++ 08_05/script.js
@@ -28,12 +28,11 @@ const greenPack = {
 
         //below action is carried out using the price value
         // Displayed in a different currency using this funtion
-        newPrice: function(price) {
-            console.log("Price in the CAD method:", this.price);
-            this.price_in_inr = price * window.inr;
-            console.log("After the method in CAD:", this.price);
-            console.log("New price in inr:", this.price_in_inr);
-        }
+        // newPrice: function(price) {
+        // console.log("Price in the CAD method:", this.price);
+        // this.price_in_inr = price * window.inr;
+        // console.log("After the method in CAD:", this.price);
+        // console.log("New price in inr:", this.price_in_inr);
     },
 };
 
--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,19 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+window.price = 1000;
+window.inr = 55;
+
+function tax(price, tax = 0.13) {
+    let finalPrice = price * tax + price;
+    return finalPrice;
+}
+const laptop = {
+    name: "hp",
+    model: "spectre",
+    number: "X360",
+    tilt_feature: true,
+    size: 14,
+    price: 999,
+    tax: 0.13,
+};
