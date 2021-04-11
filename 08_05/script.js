8e077fd48fcea4833291744132f7e37063bdd883
Naren <prasannanarayanan2000@gmail.com>
2021-04-10 20:51:50 -0400

    commit

--- 08_05/script.js
+++ 08_05/script.js
@@ -21,16 +21,19 @@ const greenPack = {
         console.log("this.volume in the method:", this.volume);
         this.volume = volume;
         console.log("this.volume in the method UPDATE:", this.volume);
-        (function() {
-            console.log("this.volume in nested function:", this.volume);
-        })();
-    },
-    newPrice: function(price) {
-        console.log("Price in the CAD method:", this.price);
+        //the second volume assigned refers to the window obj volume 20
+        // (function() {
+        //     console.log("this.volume in nested function:", this.volume);
+        // })();
 
-        this.price_in_inr = price * window.inr;
-        console.log("After the method in CAD:", this.price);
-        console.log("New price in inr:", this.price_in_inr);
+        //below action is carried out using the price value
+        // Displayed in a different currency using this funtion
+        // newPrice: function(price) {
+        //     console.log("Price in the CAD method:", this.price);
+        //     this.price_in_inr = price * window.inr;
+        //     console.log("After the method in CAD:", this.price);
+        //     console.log("New price in inr:", this.price_in_inr);
+        // }
     },
 };
 
